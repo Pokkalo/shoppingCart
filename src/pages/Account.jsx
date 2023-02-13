18e5eb65a1e816c30ee6fa9cf7e2f1d38399947d
t40776168 <pt368756721@gmail.com>
2023-02-13 18:32:50 +0800

    test1

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,7 +5,6 @@ import { useNavigate, } from 'react-router-dom';
 
 import { auth } from '../firebase-config';
 import {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
